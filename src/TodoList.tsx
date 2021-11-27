228eaa8cac41ea86d0c3e676e541ae8ddb3032d7
jp_knj <kenji.tomita1996@gmail.com>
2021-11-27 21:35:49 +0900

    :+1: add todolist component and set atom state

---
+++ src/TodoList.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import {
+    RecoilRoot,
+    atom,
+    selector,
+    useRecoilState,
+    useRecoilValue,
+} from 'recoil';
+
+const todoListState = atom({
+    key: 'todoListState',
+    default: [],
+});
+
+function TodoList() {
+    const todoList = useRecoilValue(todoListState);
+
+    return (
+        <>
+            {/* <TodoListStats /> */}
+            {/* <TodoListFilters /> */}
+            <TodoItemCreator />
+
+            {todoList.map((todoItem) => (
+                <TodoItem key={todoItem.id} item={todoItem} />
+            ))}
+        </>
+    );
+}
+
+export default TodoList;
