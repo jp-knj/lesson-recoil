d836e22c4f525b78dab0a9eec5deb65a81ad43de
jp_knj <kenji.tomita1996@gmail.com>
2021-11-28 12:09:27 +0900

    :sparkles: using recoil atom and store shareable state

--- demo/src/examples/Atoms.tsx
+++ demo/src/examples/Atoms.tsx
@@ -1,16 +1,29 @@
 import {useState} from "react";
+import {atom, useRecoilState, useRecoilValue} from 'recoil';
+
+const darkModeAtom = atom({
+    key: 'darkMode',
+    default: false
+})
 
 const DarkModeSwitch = () => {
-    const [darkMode, setDarkMode] = useState<boolean>(false)
+    const [darkMode, setDarkMode] = useRecoilState<boolean>(darkModeAtom)
     console.log('darkMode', darkMode)
     const handleChange = (ev: React.ChangeEvent<HTMLInputElement>) => {
         setDarkMode(ev.currentTarget.checked)
     }
     return <input type='checkbox' onChange={handleChange}/>
 }
+
 const Button = () => {
-    return <button>UI button</button>
+    const darkMode = useRecoilValue<boolean>(darkModeAtom)
+    const color = {
+        backgroundColor: darkMode ? 'black': 'white',
+       color: darkMode ? 'white' : 'black'
+    }
+    return <button style={color}>UI button</button>
 }
+
 export  const Atoms = () => {
     return (
         <>
