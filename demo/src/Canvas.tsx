ac4c019389ac110291ad553df0c758788a425526
jp_knj <kenji.tomita1996@gmail.com>
2021-11-30 08:57:16 +0900

    :sparkles: learn about atom family

--- demo/src/Canvas.tsx
+++ demo/src/Canvas.tsx
@@ -1,75 +1,33 @@
-import {createContext, useState} from 'react'
-import {atom, useSetRecoilState} from "recoil";
-import {Element, Rectangle} from './components/Rectangle/Rectangle'
+import {atom, useRecoilValue, useSetRecoilState} from "recoil";
+import {Rectangle} from './components/Rectangle/Rectangle'
 import {PageContainer} from './PageContainer'
 import {Toolbar} from './Toolbar'
-
-type ElementsContextType = {
-    elements: Element[]
-    addElement: () => void
-    setElement: SetElement
-}
-
-export const ElementsContext = createContext<ElementsContextType>({
-    elements: [],
-    addElement: () => {},
-    setElement: () => {},
-})
-
-type SelectedElementContextType = {
-    selectedElement: number | null
-    setselectedelement: (index:number) => void
-}
 
 export const selectedElementState = atom<number | null>({
     key: 'selectedElement',
     default: null
 })
 
-export type SetElement = (indextoset: number, newelement: Element) => void
+export const elementsState = atom<number[]>({
+    key: 'elements',
+    default: []
+})
 
 function Canvas() {
-    const [elements, setElements] = useState<Element[]>([])
-    const [selectedelement, setselectedelement] = useState<number | null>(null)
-
-    const setElement: SetElement = (indexToSet, newElement) => {
-        setElements(
-            elements.map((element, index) => {
-                if (indexToSet === index) return newElement
-                return element
-            }),
-        )
-    }
-
-    const addElement = () => {
-        setElements((elements) => {
-            return [
-                ...elements,
-                {
-                    style: {
-                        position: {top: 100 + elements.length * 30, left: 100 + elements.length * 30},
-                        size: {width: 100, height: 100},
-                    },
-                },
-            ]
-        })
-    }
-
     const setSelectedElement = useSetRecoilState(selectedElementState)
+    const elements = useRecoilValue(elementsState)
 
     return (
-            <ElementsContext.Provider value={{elements, addElement, setElement}}>
                 <PageContainer
                     onClick={() => {
                         setSelectedElement(null)
                     }}
                 >
                     <Toolbar />
-                    {elements.map((element, index) => (
-                        <Rectangle key={index} element={element} index={index} />
+                    {elements.map((id, index) => (
+                        <Rectangle key={index} id={id} />
                     ))}
                 </PageContainer>
-            </ElementsContext.Provider>
     )
 }
 
